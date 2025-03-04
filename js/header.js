665de202ecc485eb74c23948e4d2d83490d2ead3
ShahzebMubashar <shahzebmubashar7@gmail.com>
2025-03-04 18:55:04 +0500

    fixed navbar on small devices

--- js/header.js
+++ js/header.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
 
     menuToggle.addEventListener("click", function () {
-        this.classList.toggle("active");
         navLinks.classList.toggle("active");
+        menuToggle.classList.toggle("active");
     });
 });
